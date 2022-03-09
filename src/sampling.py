4b99b681e1f15d7aae31ff33ff6007f4d9dc47ec
JJ19990510 <94134681+JJ19990510@users.noreply.github.com>
2022-03-09 20:55:10 +0800

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -141,6 +141,52 @@ def mnist_noniid_unequal(dataset, num_users):
 
     return dict_users
 
+def carvana_image_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from carvana_image dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items,
+                                             replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+
+def carvana_image_noniid(dataset, num_users):
+    """
+    Sample non-I.I.D client data from carvana_image dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+    # 5,088 training imgs -->  53 imgs/shard X 96 shards
+    num_shards, num_imgs = 53, 96
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([]) for i in range(num_users)}
+    idxs = np.arange(num_shards*num_imgs)
+    # labels = dataset.train_labels.numpy()
+    labels = np.array(dataset.train_labels)
+
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+
+    # divide and assign
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate(
+                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+
+    return dict_users
+
 
 def cifar_iid(dataset, num_users):
     """
@@ -156,7 +202,6 @@ def cifar_iid(dataset, num_users):
                                              replace=False))
         all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
-
 
 def cifar_noniid(dataset, num_users):
     """
@@ -184,6 +229,7 @@ def cifar_noniid(dataset, num_users):
         for rand in rand_set:
             dict_users[i] = np.concatenate(
                 (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+
     return dict_users
 
 
