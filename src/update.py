6d4fdd3707d0f0f862fa6fd2f6388c306952d395
JJ19990510 <94134681+JJ19990510@users.noreply.github.com>
2022-05-16 00:29:02 +0800

    Update update.py

--- src/update.py
+++ src/update.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Python version: 3.6
+import sys
 
+from tqdm import tqdm
 import torch
-import numpy as np
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
+from my_utils import loss_function
 import warnings
+
 warnings.filterwarnings("ignore")
 
+
 class SegmentationMetric(nn.Module):
-    def __init__(self, NUM_CLASSES, use_cuda):
+    def __init__(self, NUM_CLASSES, device):
         super().__init__()
         self.numClass = NUM_CLASSES
-        self.use_cuda = use_cuda
+        self.device = device
         self.confusionMatrix = torch.zeros((self.numClass, self.numClass), dtype=torch.int64)
-        if self.use_cuda:
-            self.confusionMatrix = self.confusionMatrix#.cuda() (cpu的话就注释掉)
+        self.confusionMatrix = self.confusionMatrix.to(device)
 
     def pixelAccuracy(self):
         # return all class overall pixel accuracy
@@ -50,8 +53,8 @@ class SegmentationMetric(nn.Module):
 
     def meanPixelAccuracy(self):
         classAcc = self.classPixelAccuracy()
-        meanAcc = torch.mean(classAcc)  # np.nanmean 求平均值，nan表示遇到Nan类型，其值取为0
-        return meanAcc  # 返回单个值，如：np.nanmean([0.90, 0.80, 0.96, nan, nan]) = (0.90 + 0.80 + 0.96） / 3 =  0.89
+        meanAcc = torch.mean(classAcc)  # np.nanmean Calculate the average value. Nan indicates that Nan type is encountered, and its value is 0
+        return meanAcc  # Returns a single value, such as：np.nanmean([0.90, 0.80, 0.96, nan, nan]) = (0.90 + 0.80 + 0.96） / 3 =  0.89
 
     def meanIntersectionOverUnion(self):
         IoU = self.IntersectionOverUnion()
@@ -61,13 +64,13 @@ class SegmentationMetric(nn.Module):
     def IntersectionOverUnion(self):
         # Intersection = TP Union = TP + FP + FN
         # IoU = TP / (TP + FP + FN)
-        intersection = torch.diag(self.confusionMatrix)  # 取对角元素的值，返回列表
+        intersection = torch.diag(self.confusionMatrix)  # Take the value of the diagonal element and return to the list
         union = torch.sum(self.confusionMatrix, dim=1) + torch.sum(self.confusionMatrix, dim=0) - torch.diag(
-            self.confusionMatrix)  # axis = 1表示混淆矩阵行的值，返回列表； axis = 0表示取混淆矩阵列的值，返回列表
-        IoU = intersection / union  # 返回列表，其值为各个类别的IoU
+            self.confusionMatrix)  # axis = 1 indicates the value of the confusion matrix row and returns the list; Axis = 0 means to take the value of the confusion matrix column and return the list
+        IoU = intersection / union  # Returns a list whose value is IoU of each category
         return IoU
 
-    def genConfusionMatrix(self, imgPredict, imgLabel):  # 同FCN中score.py的fast_hist()函数
+    def genConfusionMatrix(self, imgPredict, imgLabel):  # Same as fast_hist() function in score.py of FCN
         # remove classes from unlabeled pixels in gt image and predict
         # mask = (imgLabel >= 0) & (imgLabel < self.numClass)
         # label = self.numClass * imgLabel[mask] + imgPredict[mask]
@@ -79,7 +82,7 @@ class SegmentationMetric(nn.Module):
 
         return confusionMatrix
 
-    def getConfusionMatrix(self):  # 同FCN中score.py的fast_hist()函数
+    def getConfusionMatrix(self):  # Same as fast_hist() function in score.py of FCN
         # cfM = self.confusionMatrix / np.sum(self.confusionMatrix, axis=0)
         cfM = self.confusionMatrix
         return cfM
@@ -93,23 +96,20 @@ class SegmentationMetric(nn.Module):
         FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
         return FWIoU
 
-
-
     def MeanIOU(self):
         IOU = self.IntersectionOverUnion()
         return IOU, torch.mean(IOU)
 
     def addBatch(self, imgPredict, imgLabel):
-        imgPredict = imgPredict.squeeze(1)  # .cpu().numpy().astype('uint8')
-        imgLabel = imgLabel.squeeze(1)  # .cpu().numpy().astype('uint8')
+        # imgPredict = imgPredict.squeeze(1)  # .cpu().numpy().astype('uint8')
+        # imgLabel = imgLabel.squeeze(1)  # .cpu().numpy().astype('uint8')
         assert imgPredict.shape == imgLabel.shape
         self.confusionMatrix += self.genConfusionMatrix(imgPredict, imgLabel)  # .float()
         return self.confusionMatrix
 
     def reset(self):
         self.confusionMatrix = torch.zeros((self.numClass, self.numClass))
-        if self.use_cuda:
-            self.confusionMatrix = self.confusionMatrix.cuda()
+        self.confusionMatrix = self.confusionMatrix.to(self.device)
 
 
 class DatasetSplit(Dataset):
@@ -129,18 +129,15 @@ class DatasetSplit(Dataset):
 
 
 class LocalUpdate(object):
-    def __init__(self, args, dataset, idxs, logger):
+    def __init__(self, args, dataset, idxs, logger, print=print):
         self.args = args
         self.logger = logger
         self.trainloader, self.validloader, self.testloader = self.train_val_test(
             dataset, list(idxs))
-        self.device = 'cuda' if args.gpu is not None else 'cpu'
+        # self.device = 'cuda' if args.gpu else 'cpu'
+        self.device = self.args.gpu
+        self.print = print
         # Default criterion set to NLL loss function
-        if args.criterion == 'CE':
-            self.criterion = torch.nn.CrossEntropyLoss()
-        elif args.criterion == 'NLLLoss':
-            self.criterion = torch.nn.NLLLoss().to(self.device)
-
         # self.criterion = nn.NLLLoss().to(self.device)
 
     def train_val_test(self, dataset, idxs):
@@ -149,24 +146,16 @@ class LocalUpdate(object):
         and user indexes.
         """
         # split indexes for train, validation, and test (80, 10, 10)
-        idxs_train = idxs[:int(0.8*len(idxs))]
-        idxs_val = idxs[int(0.8*len(idxs)):int(0.9*len(idxs))]
-        idxs_test = idxs[int(0.9*len(idxs)):]
-        # print(f"len(idxs) = {len(idxs)}")
-        # print(f"len(idxs_train) = {len(idxs_train)}")
-        # print(f"len(idxs_val) = {len(idxs_val)}")
-        # print(f"len(idxs_test) = {len(idxs_test)}")
-        #
-        # print(f"self.args.local_bs = {self.args.local_bs}")
-        # print(f"int(len(idxs_val)/10) = {int(len(idxs_val)/10)}")
-        # print(f"int(len(idxs_test)/10) = {int(len(idxs_test)/10)}")
+        idxs_train = idxs[:int(0.8 * len(idxs))]
+        idxs_val = idxs[int(0.8 * len(idxs)):int(0.9 * len(idxs))]
+        idxs_test = idxs[int(0.9 * len(idxs)):]
 
         trainloader = DataLoader(DatasetSplit(dataset, idxs_train),
                                  batch_size=self.args.local_bs, shuffle=True)
         validloader = DataLoader(DatasetSplit(dataset, idxs_val),
-                                 batch_size=int(len(idxs_val)/10) if int(len(idxs_val)/10) > 0 else 1, shuffle=False)
+                                 batch_size=int(len(idxs_val) / 10), shuffle=False)
         testloader = DataLoader(DatasetSplit(dataset, idxs_test),
-                                batch_size=int(len(idxs_test)/10) if int(len(idxs_test)/10) > 0 else 1, shuffle=False)
+                                batch_size=int(len(idxs_test) / 10), shuffle=False)
         return trainloader, validloader, testloader
 
     def update_weights(self, model, global_round):
@@ -181,30 +170,35 @@ class LocalUpdate(object):
         elif self.args.optimizer == 'adam':
             optimizer = torch.optim.Adam(model.parameters(), lr=self.args.lr,
                                          weight_decay=1e-4)
-
+        metric = SegmentationMetric(self.args.Metric_num_classes, self.args.gpu).to(self.args.gpu)
         for iter in range(self.args.local_ep):
+            metric.reset()
             batch_loss = []
             for batch_idx, (images, labels) in enumerate(self.trainloader):
                 images, labels = images.to(self.device), labels.to(self.device)
 
                 model.zero_grad()
                 log_probs = model(images)
-                if self.args.criterion == 'CE':
-                    loss = self.criterion(log_probs.float(), labels.long())
-                elif self.args.criterion == 'NLLLoss':
-                    loss = self.criterion(log_probs, labels)
                 # loss = self.criterion(log_probs, labels)
+                loss = loss_function(self.args, log_probs, labels, self.args.gpu)
+                metric.addBatch(log_probs.squeeze(1) > 0.5, labels)  # [batch_size, width, height]
                 loss.backward()
                 optimizer.step()
 
-                if self.args.verbose and (batch_idx % 10 == 0):
-                    print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        global_round, iter, batch_idx * len(images),
-                        len(self.trainloader.dataset),
-                        100. * batch_idx / len(self.trainloader), loss.item()))
+                if self.args.verbose and ((batch_idx + 1) % 4 == 0):
+                    self.print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        global_round + 1,
+                        iter + 1,
+                        # batch_idx * len(images),
+                        batch_idx + 1,
+                        # len(self.trainloader.dataset),
+                        len(self.trainloader),
+                        100. * (batch_idx + 1) / len(self.trainloader),
+                        loss.item()))
                 self.logger.add_scalar('loss', loss.item())
                 batch_loss.append(loss.item())
-            epoch_loss.append(sum(batch_loss)/len(batch_loss))
+
+            epoch_loss.append(sum(batch_loss) / len(batch_loss))
 
         return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
 
@@ -214,27 +208,25 @@ class LocalUpdate(object):
 
         model.eval()
         loss, total, correct = 0.0, 0.0, 0.0
-        metric = SegmentationMetric(self.args.num_classes, self.device).to(self.device)
+        metric = SegmentationMetric(self.args.Metric_num_classes, self.args.gpu).to(self.args.gpu)
         for batch_idx, (images, labels) in enumerate(self.testloader):
             images, labels = images.to(self.device), labels.to(self.device)
 
             # Inference
             outputs = model(images)
-            if self.args.criterion == 'CE':
-                batch_loss = self.criterion(outputs.float(), labels.long())
-            elif self.args.criterion == 'NLLLoss':
-                batch_loss = self.criterion(outputs, labels)
             # batch_loss = self.criterion(outputs, labels)
+            batch_loss = loss_function(self.args, outputs, labels, self.args.gpu)
             loss += batch_loss.item()
 
             if self.args.dataset == "carvana_image":
-                metric.addBatch(outputs.argmax(1), labels)
+                metric.addBatch(outputs.squeeze(1) > 0.5, labels)  # [batch_size, width, height]
             else:
                 # Prediction
                 _, pred_labels = torch.max(outputs, 1)
                 pred_labels = pred_labels.view(-1)
                 correct += torch.sum(torch.eq(pred_labels, labels)).item()
                 total += len(labels)
+
         if self.args.dataset == "carvana_image":
             IOU, meanIOU = metric.MeanIOU()
             IOU = IOU.cpu().detach().numpy()
@@ -245,46 +237,43 @@ class LocalUpdate(object):
             return accuracy, loss
 
 
-def test_inference(args, model, test_dataset):
+def test_inference(args, model, test_dataset, device):
     """ Returns the test accuracy and loss.
     """
 
     model.eval()
     loss, total, correct = 0.0, 0.0, 0.0
 
-    device = 'cuda' if args.gpu else 'cpu'
-    if args.criterion == 'CE':
-        criterion = torch.nn.CrossEntropyLoss()
-    elif args.criterion == 'NLLLoss':
-        criterion = torch.nn.NLLLoss().to(device)
+    # device = 'cuda' if args.gpu else 'cpu'
+    # if args.criterion == 'BCE':
+    #     criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([3.]).to(device)).to(device)
+    # elif args.criterion == 'NLLLoss':
+    #     criterion = torch.nn.NLLLoss().to(device)
 
-    testloader = DataLoader(test_dataset, batch_size=args.testloder_BatchSize,
+    testloader = DataLoader(test_dataset,
+                            batch_size=args.testloder_BatchSize,
                             shuffle=False)
 
-    metric = SegmentationMetric(args.num_classes, device).to(device)
+    metric = SegmentationMetric(args.Metric_num_classes, device).to(device)  # NUM_CLASSES表示有NUM_CLASSES个分类，有几个分类就填几
 
-    for batch_idx, (images, labels) in enumerate(testloader):
+    pbar = tqdm(enumerate(testloader), total=len(testloader), file=sys.stdout, unit='sample', ncols=args.ncols)
+    for batch_idx, (images, labels) in pbar:
         images, labels = images.to(device), labels.to(device)
 
         # Inference
         outputs = model(images)
-        if args.criterion == 'CE':
-            batch_loss = criterion(outputs.float(), labels.long())
-        elif args.criterion == 'NLLLoss':
-            batch_loss = criterion(outputs, labels)
-
+        batch_loss = loss_function(args, outputs, labels, device)
         loss += batch_loss.item()
 
         if args.dataset == "carvana_image":
-            # print(f"outputs.argmax(1).shape = {outputs.argmax(1).shape}")
-            # print(f"labels.shape = {labels.shape}")
-            metric.addBatch(outputs.argmax(1), labels)
+            metric.addBatch(outputs.squeeze(1) > 0.5, labels)  # [batch_size, width, height]
         else:
             # Prediction
             _, pred_labels = torch.max(outputs, 1)
             pred_labels = pred_labels.view(-1)
             correct += torch.sum(torch.eq(pred_labels, labels)).item()
             total += len(labels)
+
     if args.dataset == "carvana_image":
         IOU, meanIOU = metric.MeanIOU()
         IOU = IOU.cpu().detach().numpy()
